1562f06e6be6045e654fa755f3cfa1b54051de48
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-26 13:16:16 +0300

    prettier

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,31 +1,30 @@
-import { ColorContext } from '../../Context/ColorContext/colorContext';
-import { StyledNavbar } from './Navbar.styled';
-import { StyledMobile } from './Mobile.styled';
-import { useContext } from 'react';
-import Search from '../Search/Search';
+import { ColorContext } from '../../Context/ColorContext/colorContext'
+import { StyledNavbar } from './Navbar.styled'
+import { StyledMobile } from './Mobile.styled'
+import { useContext } from 'react'
+import Search from '../Search/Search'
 
 const Navbar = () => {
-  const theme = useContext(ColorContext);
-  console.log(theme);
+  const theme = useContext(ColorContext)
 
   return (
     <>
       <StyledNavbar theme={theme}>
-        <div className='logo'>
+        <div className="logo">
           <h1>TnC</h1>
         </div>
-        <div className='search'>
+        <div className="search">
           <Search />
         </div>
-        <div className='Landlord'>
+        <div className="Landlord">
           <h1>Become a Landlord</h1>
         </div>
       </StyledNavbar>
-      <StyledMobile theme={theme} >
+      <StyledMobile theme={theme}>
         <Search />
       </StyledMobile>
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
