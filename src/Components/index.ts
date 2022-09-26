f814fa8e3115d4430b5a356bd850417d71057071
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-26 13:16:04 +0300

    prettier

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,2 +1,2 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as StyledApp } from './App/App.styled';
+export { default as Navbar } from './Navbar/Navbar'
+export { default as StyledApp } from './App/App.styled'
