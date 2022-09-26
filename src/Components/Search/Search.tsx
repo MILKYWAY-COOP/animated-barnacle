c68385b1fcc56cdc60f966d20c9b06de50a63b37
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-26 13:16:22 +0300

    prettier

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,40 +1,40 @@
-import { ColorContext } from '../../Context/ColorContext/colorContext';
-import { StyledSearch } from './Search.styled';
-import { FaSearch } from 'react-icons/fa';
-import { useContext } from 'react';
+import { ColorContext } from '../../Context/ColorContext/colorContext'
+import { StyledSearch } from './Search.styled'
+import { FaSearch } from 'react-icons/fa'
+import { useContext } from 'react'
 
 const Search = () => {
-  const theme = useContext(ColorContext);
+  const theme = useContext(ColorContext)
   return (
     <StyledSearch theme={theme}>
       <select>
-        <option value='1'>Place</option>
-        <option value='2'>Mwangaza</option>
-        <option value='3'>Area 4</option>
-        <option value='4'>VM</option>
-        <option value='5'>Catholic</option>
+        <option value="1">Place</option>
+        <option value="2">Mwangaza</option>
+        <option value="3">Area 4</option>
+        <option value="4">VM</option>
+        <option value="5">Catholic</option>
       </select>
       <select>
-        <option value='1'>$$$</option>
-        <option value='2'> Below 3000</option>
-        <option value='3'>3000 - 4000</option>
-        <option value='4'>4000 - 5000</option>
-        <option value='4'>5000 - 6000</option>
-        <option value='4'>6000 - 7000</option>
-        <option value='4'>Above 7000</option>
+        <option value="1">$$$</option>
+        <option value="2"> Below 3000</option>
+        <option value="3">3000 - 4000</option>
+        <option value="4">4000 - 5000</option>
+        <option value="4">5000 - 6000</option>
+        <option value="4">6000 - 7000</option>
+        <option value="4">Above 7000</option>
       </select>
       <select>
-        <option value='0'>Type</option>
-        <option value='1'>Bed Sitter</option>
-        <option value='2'>Single Room</option>
-        <option value='3'>One Bed Room</option>
-        <option value='4'>Two Bed Room</option>
+        <option value="0">Type</option>
+        <option value="1">Bed Sitter</option>
+        <option value="2">Single Room</option>
+        <option value="3">One Bed Room</option>
+        <option value="4">Two Bed Room</option>
       </select>
-      <div className='btn'>
+      <div className="btn">
         <FaSearch />
       </div>
     </StyledSearch>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
