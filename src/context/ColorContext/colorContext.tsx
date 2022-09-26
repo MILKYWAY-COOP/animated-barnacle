cab607abcf3c35980fa3061ea131061b03bd1556
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-26 13:16:26 +0300

    prettier

--- src/context/ColorContext/colorContext.tsx
+++ src/context/ColorContext/colorContext.tsx
@@ -1,20 +1,17 @@
-import { useState, createContext } from 'react';
+import { useState, createContext } from 'react'
 
-import { Colors, IColors } from '../../Data/Colors';
+import { Colors, IColors } from '../../Data/Colors'
 
-export const ColorContext = createContext<IColors | null>(Colors.darkTheme);
+export const ColorContext = createContext<IColors | null>(Colors.darkTheme)
 
 // create an interface for the children
 interface Props {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
-
 export const ColorProvider = ({ children }: Props) => {
-  const { lightTheme, darkTheme } = Colors;
-  const [theme, setTheme] = useState<IColors>(darkTheme);
+  const { lightTheme, darkTheme } = Colors
+  const [theme, setTheme] = useState<IColors>(darkTheme)
 
-  return (
-    <ColorContext.Provider value={theme}>{children}</ColorContext.Provider>
-  );
-};
+  return <ColorContext.Provider value={theme}>{children}</ColorContext.Provider>
+}
