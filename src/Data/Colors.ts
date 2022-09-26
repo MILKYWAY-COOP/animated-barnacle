8d113676bf1fbe218ca92fb0d0b3900fd272d90c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-26 11:22:29 +0300

    Navbar

--- src/Data/Colors.ts
+++ src/Data/Colors.ts
@@ -2,14 +2,18 @@ const darkTheme = {
   mainColor: '#040348',
   secondaryColor: '#082567',
   fontColor: '#FFFFFF',
-  secondaryFontColor: '#FFFDD0'
+  secondaryFontColor: '#FFFDD0',
+  universal1: '#FFFDD0',
+  fontFamily: `'Reem Kufi Ink', sans-serif`
 };
 
 const lightTheme = {
   mainColor: '#ADD8E6',
   secondaryColor: '#8AC7DB',
   fontColor: '#000000',
-  secondaryFontColor: '#282829'
+  secondaryFontColor: '#282829',
+  fontFamily: `'Reem Kufi Ink', sans-serif`,
+  universal1: '#FFFDD0',
 };
 
 export const Colors = {
