67d86f0c64b788fe3026fd5f28526e74d059892b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-27 13:12:27 +0300

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Navbar, StyledApp } from './Components'
+import { AuthRoutes, ROUTES } from './Routes'
+import { ReactNode, useContext } from 'react'
+import { ColorContext } from './Context'
+
+const Wrapper = ({ children }: { children: ReactNode }) => {
+  const theme = useContext(ColorContext)
+  return (
+    <>
+      <Navbar />
+      <StyledApp theme={theme}>{children}</StyledApp>
+    </>
+  )
+}
 
 function App() {
   return (
-    <StyledApp>
-      <Navbar />
-    </StyledApp>
+    <Wrapper>
+      <ROUTES />
+    </Wrapper>
   )
 }
 
