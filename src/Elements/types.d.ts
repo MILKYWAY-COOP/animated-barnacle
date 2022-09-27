56271e370f1e23e4e402d5fa88927b136237fa23
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-27 16:58:21 +0300

    Context

--- src/Elements/types.d.ts
+++ src/Elements/types.d.ts
@@ -2,3 +2,30 @@ export interface ITheme {
   isLogged: boolean
   setLogged: (Logged: boolean) => void
 }
+
+export const Colors = {
+  darkTheme,
+  lightTheme,
+}
+
+// create interface for colors
+export interface IColors {
+  mainColor: string
+  secondaryColor: string
+  fontColor: string
+  secondaryFontColor: string
+}
+
+// create interface for theme
+export interface ITheme {
+  IColors
+  setTheme: () => void
+}
+
+export interface IUserContext {
+  user: {}
+  isLoggedIn: boolean
+  createUser: (user: {}) => void
+  logOut: () => void
+  signInWithGoogle: () => void
+}
