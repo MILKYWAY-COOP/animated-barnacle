d0f83416916874857dd65f3c0908819ac87ad317
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-27 16:58:16 +0300

    Context

--- src/context/ColorContext/colorContext.tsx
+++ src/context/ColorContext/colorContext.tsx
@@ -1,8 +1,8 @@
 import { useState, createContext } from 'react'
-
 import { Colors, IColors } from '../../Data/Colors'
+import { ITheme } from '../../Elements/types'
 
-export const ColorContext = createContext<IColors | null>(Colors.darkTheme)
+export const ColorContext = createContext<IColors | ITheme | null>(Colors.darkTheme)
 
 // create an interface for the children
 interface Props {
@@ -13,7 +13,7 @@ export const ColorProvider = ({ children }: Props) => {
   const { lightTheme, darkTheme } = Colors
   const [theme, setTheme] = useState<IColors>(darkTheme)
 
-  return <ColorContext.Provider value={theme}>{children}</ColorContext.Provider>
+  return <ColorContext.Provider value={theme }>{children}</ColorContext.Provider>
 }
 
 export default ColorProvider
