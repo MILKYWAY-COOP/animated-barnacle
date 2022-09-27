dd387d54aa0c0e5d5e923480530028f520944654
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-27 13:12:33 +0300

    Login page

--- src/Components/App/App.styled.ts
+++ src/Components/App/App.styled.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 const StyledApp = styled.div`
   width: 100vw;
   height: 100vh;
+  background: ${({ theme }) => theme.secondaryColor};
+  padding: 1em;
 `
 
 export default StyledApp
