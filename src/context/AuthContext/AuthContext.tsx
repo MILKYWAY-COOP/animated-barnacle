e7f44ce1a91623d2f96110f4415d64a1e26779a0
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 06:29:10 +0300

    Login

--- src/context/AuthContext/AuthContext.tsx
+++ src/context/AuthContext/AuthContext.tsx
@@ -9,6 +9,7 @@ import {
 } from 'firebase/auth'
 import { auth } from '../Firebase'
 import { IUserContext } from '../../Elements/types'
+import { useNavigate } from 'react-router-dom'
 
 const AuthContext = createContext<IUserContext>({} as IUserContext)
 export const useData = () => useContext(AuthContext)
@@ -20,10 +21,14 @@ interface Props {
 export const AuthContextProvider = ({ children }: Props) => {
   const [user, setUser] = useState({})
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [errorMSG, setErrorMSG] = useState('')
+  const navigate = useNavigate()
 
   const createUser = async (email: string, password: string) => {
     try {
       await createUserWithEmailAndPassword(auth, email, password)
+      setIsLoggedIn(true)
+      navigate('/')
     } catch (error) {
       console.log(error)
     }
@@ -33,6 +38,8 @@ export const AuthContextProvider = ({ children }: Props) => {
     const provider = new GoogleAuthProvider()
     try {
       await signInWithPopup(auth, provider)
+      setIsLoggedIn(true)
+      navigate('/')
     } catch (error) {
       console.log(error)
     }
@@ -41,8 +48,10 @@ export const AuthContextProvider = ({ children }: Props) => {
   const signIn = async (email: string, password: string) => {
     try {
       await signInWithEmailAndPassword(auth, email, password)
+      setIsLoggedIn(true)
+      navigate('/')
     } catch (error) {
-      console.log(error)
+      setErrorMSG('Invalid Email or Password')
     }
   }
 
@@ -58,10 +67,10 @@ export const AuthContextProvider = ({ children }: Props) => {
     const unsubscribe = onAuthStateChanged(auth, user => {
       if (user) {
         setUser(user)
-        setIsLoggedIn(true)
+        //setIsLoggedIn(true)
       } else {
         setUser({})
-        setIsLoggedIn(false)
+        //setIsLoggedIn(false)
       }
     })
 
@@ -70,7 +79,15 @@ export const AuthContextProvider = ({ children }: Props) => {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn, logOut, signInWithGoogle, user, createUser }}
+      value={{
+        isLoggedIn,
+        logOut,
+        signInWithGoogle,
+        user,
+        createUser,
+        signIn,
+        errorMSG,
+      }}
     >
       {children}
     </AuthContext.Provider>
