d4aef2c8039412294f87d80697da1feff73568f8
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 08:04:30 +0300

    404

--- src/Components/App/App.styled.ts
+++ src/Components/App/App.styled.ts
@@ -2,13 +2,12 @@ import styled from 'styled-components'
 
 const StyledApp = styled.div`
   width: 100vw;
-  /* height: calc(100vh - 80px); */
-  height: auto;
+  height: calc(100vh - 80px);
   background: ${({ theme }) => theme.secondaryColor};
   padding: 1em;
 
   @media (max-width: 768px) {
-    height: calc(100vh - 128px);
+    height: auto;
   }
 `
 
