ac7269ed47fa4734f6126467cb18dbac1160b684
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 06:37:08 +0300

    Updated Height

--- src/Components/App/App.styled.ts
+++ src/Components/App/App.styled.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 const StyledApp = styled.div`
   width: 100vw;
-  height: calc(100vh - 80px);
+  /* height: calc(100vh - 80px); */
+  height: auto;
   background: ${({ theme }) => theme.secondaryColor};
   padding: 1em;
 
