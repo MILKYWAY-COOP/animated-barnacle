4b8f38490217047fedc9a1f6892f3986921c729f
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 08:04:40 +0300

    404

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,1 +1,3 @@
-export {default as SignIn} from './Signin/Signin';
+export { default as SignIn } from './Signin/Signin';
+export { default as notFound } from './404/Error'
+export { default as Home } from './Home/Home'
