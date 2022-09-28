fc3abd1c605096cd554d2562159d06cf810a15c1
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 08:04:56 +0300

    404

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -10,11 +10,16 @@ export const Paths = {
   register: '/register',
   forgotPassword: '/forgot-password',
 
-  notFound: '/404',
+  notFound: '*',
 }
 
 export const AuthRoutes = () => (
-  <Routes>{/* <Route path={Paths.home} element={<Pages.Home />} /> */}</Routes>
+  <Routes>
+    <Route path={Paths.home} element={<Pages.Home />} />
+    <Route path={Paths.notFound} element={<Pages.notFound />} />
+    <Route path={Paths.register} element={<Pages.SignIn />} />
+    <Route path={Paths.login} element={<Pages.SignIn />} />
+  </Routes>
 )
 
 export const ROUTES = () => (
@@ -22,5 +27,6 @@ export const ROUTES = () => (
     <Route path={Paths.login} element={<Pages.SignIn />} />
     <Route path={Paths.register} element={<Pages.SignIn />} />
     <Route path={Paths.forgotPassword} element={<Pages.SignIn />} />
+    <Route path={Paths.notFound} element={<Pages.notFound />} />
   </Routes>
 )
