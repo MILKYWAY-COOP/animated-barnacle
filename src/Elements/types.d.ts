6b54cf66c80e572da2411354156d26b47a1c758a
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 12:50:41 +0300

    Cards

--- src/Elements/types.d.ts
+++ src/Elements/types.d.ts
@@ -31,3 +31,33 @@ export interface IUserContext {
   signIn: (email: string, password: string) => void
   errorMSG: string 
 }
+
+export interface ICard {
+  title: string
+  location: string
+  img: {
+    pic1: string
+    pic2: string
+    pic3: string
+  }
+  price: number
+  deposit: number
+  type: string
+  remainingRooms: number
+  launched: number
+  rating: number
+  url: string
+  wifi: boolean
+  commonRoom: boolean
+  beds: boolean
+  meals: boolean
+  water: boolean
+  electricity: boolean
+  swimmingPool: boolean
+  sharing: boolean
+  noOfPeople: number
+}
+
+export interface ICardProps {
+  card: ICard
+}
