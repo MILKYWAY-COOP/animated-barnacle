bea927d621eb57ff702fddf0843c7ce1c798d910
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 08:04:48 +0300

    404

---
+++ src/Pages/404/Error.tsx
@@ -0,0 +1,21 @@
+import { StyledError } from './Error.styled'
+
+const Error = () => {
+  return (
+    <StyledError>
+      <div className="left404">
+        <h1>404 PAGE NOT FOUND</h1>
+        <h2>You are not worthy...</h2>
+        <p>
+          Check that you typed the address correctly, go back to your previous
+          page or try using our site search to find something specific.
+        </p>
+      </div>
+      <div className="right404">
+        <img src="images/404.jpg" alt='404 Image'/>
+      </div>
+    </StyledError>
+  )
+}
+
+export default Error
