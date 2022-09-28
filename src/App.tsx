733c52807e82923355664f4bb68f161baf5c103a
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-28 08:04:26 +0300

    404

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navbar, StyledApp } from './Components'
 import { AuthRoutes, ROUTES } from './Routes'
 import { ReactNode, useContext } from 'react'
 import { ColorContext } from './Context'
+import { useData } from './Context'
 
 const Wrapper = ({ children }: { children: ReactNode }) => {
   const theme = useContext(ColorContext)
@@ -14,11 +15,8 @@ const Wrapper = ({ children }: { children: ReactNode }) => {
 }
 
 function App() {
-  return (
-    <Wrapper>
-      <ROUTES />
-    </Wrapper>
-  )
+  const { isLoggedIn } = useData()
+  return <Wrapper>{isLoggedIn ? <AuthRoutes /> : <ROUTES />}</Wrapper>
 }
 
 export default App
