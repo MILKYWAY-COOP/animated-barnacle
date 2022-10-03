079553db783b03575eac14c9664328747fc5c25b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-03 19:10:50 +0300

    Linter errors

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,3 +1,3 @@
 export { default as SignIn } from './Signin/Signin';
-export { default as notFound } from './404/Error'
+export { default as NotFound } from './404/Error'
 export { default as Home } from './Home/Home'
