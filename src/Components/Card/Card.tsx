d724c75522f7352637357a3756a81a58376109bc
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-04 01:56:23 +0300

    Easy

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -2,17 +2,34 @@ import { ICardProps } from '../../Elements/types'
 import Slider from '../Slider/Slider'
 import { StyledCard } from './Card.styled'
 
-// Create an interface for the props
 export interface ICard {
   hostel: ICardProps
 }
 
 export const Card = (props: any) => {
-  //loop through each hostel and console.log the img array
+  const { hostel } = props
 
   return (
     <StyledCard>
-      <Slider img={props.hostel.img} />
+      <div className='imgs'>
+        <Slider img={hostel.img} />
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>{hostel.title},</h3>
+            <h3>{hostel.location}</h3>
+          </div>
+          <div>
+            <span>{hostel.rating}</span>
+          </div>
+        </div>
+        <div>
+          <p>
+            Starting from Shs,<span>5000</span>
+          </p>
+        </div>
+      </div>
     </StyledCard>
   )
 }
