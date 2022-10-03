596c3858f67178744d20097a806dbba3e380da4d
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-04 00:01:57 +0300

    Slider

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,13 +3,11 @@ import Hostels from '../../Data/Hostels'
 
 export const Home = () => {
   // map through the hostels and render a card for each hostel
-  return (
-    <div>
-      {Hostels.map(hostel => (
-        <Card key={hostel.hostelId} hostel={hostel} />
-      ))}
-    </div>
-  )
+
+  const hostel = Hostels.map((hostel, key) => {
+    return <Card hostel={hostel} key={key} />
+  })
+  return <div>{hostel}</div>
 }
 
 export default Home
