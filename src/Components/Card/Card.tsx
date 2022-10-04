f0050aae5b5a0635cbcf4ed28c0c1bd8fb2fa76e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-04 14:25:07 +0300

    Problems

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,30 +1,34 @@
 import { ICardProps } from '../../Elements/types'
 import Slider from '../Slider/Slider'
 import { StyledCard } from './Card.styled'
+import {GrStar} from 'react-icons/gr'
 
 export interface ICard {
   hostel: ICardProps
 }
 
 export const Card = (props: any) => {
-  const { hostel } = props
+  const { hostel, theme } = props
 
   return (
-    <StyledCard>
-      <div className='imgs'>
+    <StyledCard theme={theme}>
+      <div className="imgs">
         <Slider img={hostel.img} />
       </div>
-      <div>
-        <div>
-          <div>
+
+      <div className="info">
+        <div className="upper">
+          <div className="leftDiv">
             <h3>{hostel.title},</h3>
             <h3>{hostel.location}</h3>
           </div>
-          <div>
-            <span>{hostel.rating}</span>
+
+          <div className="rightDiv">
+            <span>{`[${hostel.rating}]`}<GrStar/></span>
           </div>
         </div>
-        <div>
+
+        <div className="lower">
           <p>
             Starting from Shs,<span>5000</span>
           </p>
