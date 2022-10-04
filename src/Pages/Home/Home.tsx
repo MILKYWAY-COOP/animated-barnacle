6c2e6fcc35f168b9fb8f1595cbbb116d8fb180ad
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-04 14:25:22 +0300

    Problems

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,17 @@
 import { Card } from '../../Components'
 import Hostels from '../../Data/Hostels'
+import { ColorContext } from '../../context'
+import { useContext } from 'react'
+import { StyledHome } from './Home.styled'
 
 export const Home = () => {
+  const theme = useContext(ColorContext)
   // map through the hostels and render a card for each hostel
 
   const hostel = Hostels.map((hostel, key) => {
-    return <Card hostel={hostel} key={key} />
+    return <Card hostel={hostel} key={key} theme={theme} />
   })
-  return <div>{hostel}</div>
+  return <StyledHome>{hostel}</StyledHome>
 }
 
 export default Home
