6180191090020216018433362ac0bcd027d09527
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-04 14:25:04 +0300

    Problems

--- src/Components/Card/Card.styled.ts
+++ src/Components/Card/Card.styled.ts
@@ -1,10 +1,60 @@
 import styled from 'styled-components'
 
 export const StyledCard = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  min-width: 250px;
+  
+  .info {
+    display: block;
 
-    .imgs {
+    .upper {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      padding-top: 0.5em;
 
+      .leftDiv {
+        display: flex;
+        justify-content: flex-start;
+
+        h3 {
+          font-size: 1.2em;
+          font-weight: 600;
+          color: ${({ theme }) => theme.secondaryFontColor};
+          font-family: ${({ theme }) => theme.fontFamily};
+        }
+      }
+
+      .rightDiv {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        span {
+          font-size: 1.2em;
+          color: ${({ theme }) => theme.secondaryFontColor};
+          font-family: ${({ theme }) => theme.fontFamily};
+        }
+      }
     }
+
+    .lower {
+      padding-top: 0.5em;
+
+      p {
+        font-size: 1.2em;
+        color: ${({ theme }) => theme.secondaryFontColor};
+        font-family: ${({ theme }) => theme.fontFamily};
+
+        span {
+          font-weight: 600;
+          letter-spacing: 1.5px;
+          padding-left: 2px;
+          color: yellow;
+        }
+      }
+    }
+  }
 `
