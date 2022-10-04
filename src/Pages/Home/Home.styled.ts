db6bbce9a9c814115c887af15b956ac3aca5c74c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-04 14:25:19 +0300

    Problems

---
+++ src/Pages/Home/Home.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledHome = styled.div`
+  max-width: 100%;
+  /* height: fit-content; */
+  display: flex;
+  /* justify-content: space-evenly; */
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+  /* calculate a good width */
+`
