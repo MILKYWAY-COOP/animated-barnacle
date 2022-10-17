9a162cc43747205678147419cc86f97b6324f17e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-17 20:09:49 +0300

    Updates

--- src/Pages/HostelDetails/HostelDetails.tsx
+++ src/Pages/HostelDetails/HostelDetails.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Hostels from '../../Data/Hostels'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export const HostelDetails = () => {
-  return (
-    <div>HostelDetails</div>
-  )
+  const [hostel, setHostel] = useState({})
+  const { hostelId } = useParams()
+
+  const id = Number(hostelId)
+
+  useEffect(() => {
+    const hostel_: any = Hostels.find(item => {
+      return item.hostelId === id
+    })
+    setHostel(hostel_)
+  }, [id])
+
+  console.log(hostel)
+
+  return <div>HostelDetails</div>
 }
 
 export default HostelDetails
