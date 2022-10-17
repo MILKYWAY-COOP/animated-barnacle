868ef5b802306cad8911c97d15f9a72ecf8385b3
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-17 20:09:43 +0300

    Updates

--- src/Pages/Home/Home.styled.ts
+++ src/Pages/Home/Home.styled.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components'
 
 export const StyledHome = styled.div`
   width: 100%;
-  /* height: fit-content; */
+  height: fit-content;
+  padding: 1em;
   display: flex;
   justify-content: center;
   flex-direction: row;
   align-content: center;
   flex-wrap: wrap;
   gap: 20px;
-  /* calculate a good width */
 `
