647b752172aea1fa986e257b3901d0f009843380
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-18 13:21:51 +0300

    Changes

--- src/Pages/HostelDetails/HostelDetails.tsx
+++ src/Pages/HostelDetails/HostelDetails.tsx
@@ -1,10 +1,14 @@
 import Hostels from '../../Data/Hostels'
 import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { ICard } from '../../Elements/types'
+import { ColorContext } from '../../Context'
+import { StyledHostel } from './Hostel.styled'
 
 export const HostelDetails = () => {
-  const [hostel, setHostel] = useState({})
+  const [hostel, setHostel] = useState<ICard>()
   const { hostelId } = useParams()
+  const theme = useContext(ColorContext)
 
   const id = Number(hostelId)
 
@@ -15,9 +19,29 @@ export const HostelDetails = () => {
     setHostel(hostel_)
   }, [id])
 
-  console.log(hostel)
-
-  return <div>HostelDetails</div>
+  return (
+    <StyledHostel theme={theme}>
+      <div className="top">
+        <div className="title">
+          <h1>{hostel?.title}</h1>
+        </div>
+        <div className="est">
+          <h1>
+            Established: <i>{hostel?.launched}</i>
+          </h1>
+        </div>
+      </div>
+      <div className="images">
+        <div className="left">
+          <img src={`${hostel?.img?.pic2}`} alt="hostel" />
+        </div>
+        <div className="right">
+          <img src={`${hostel?.img?.pic1}`} alt="hostel" />
+          <img src={`${hostel?.img?.pic3}`} alt="hostel" />
+        </div>
+      </div>
+    </StyledHostel>
+  )
 }
 
 export default HostelDetails
