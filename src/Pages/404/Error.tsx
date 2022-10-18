ca4263d0afbc2cabb60e8ae2435740686a74ccb8
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-18 13:21:38 +0300

    Changes

--- src/Pages/404/Error.tsx
+++ src/Pages/404/Error.tsx
@@ -4,7 +4,8 @@ const Error = () => {
   return (
     <StyledError>
       <div className="left404">
-        <h1>404 PAGE NOT FOUND</h1>
+        <h1>404</h1>
+        <h1>PAGE NOT FOUND</h1>
         <h2>You are not worthy...</h2>
         <p>
           Check that you typed the address correctly, go back to your previous
