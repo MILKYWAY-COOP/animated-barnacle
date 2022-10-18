ee4d365f2b546ca8083ada712a93e4df9ef39cc2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-19 01:13:05 +0300

    Updates

---
+++ src/Pages/HostelDetails/HostelDetails.styled.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+
+export const StyledHostel = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  gap: 10px;
+
+  .top {
+    width: 100%;
+    padding-top: 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-weight: 600;
+    font-family: ${({ theme }) => theme.fontFamily};
+    color: ${({ theme }) => theme.universal1};
+
+    h1 {
+      font-size: 2rem;
+    }
+  }
+
+  .images {
+    width: calc(100% - 5em);
+    height: 300px;
+    display: grid;
+    grid-template-columns: 50% 30%;
+    justify-content: center;
+    gap: .5em;
+
+    .left {
+      width: 100%;
+      height: calc(400px + .5em);
+
+      img {
+        width: 100%;
+        height: 100% !important;
+        border-radius: 1em 0 0 1em;
+      }
+    }
+    .right {
+      width: 100%;
+      height: 400px;
+      display: grid;
+      grid-template-rows: 50% 50%;
+      gap: .5em;
+
+      img {
+        width: 100%;
+        height: 100% !important;
+      }
+      .one {
+        border-radius: 0 1em 0 0;
+      }
+      .two {
+        border-radius: 0 0 1em 0;
+      }
+    }
+  }
+
+  .bottom {
+    width: 100%;
+    height: fit-content;
+  }
+
+`
