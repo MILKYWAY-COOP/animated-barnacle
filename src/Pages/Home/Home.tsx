870a328ef1a9d334f8c9ad456db813c93a76c966
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-19 01:12:59 +0300

    Updates

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,11 +6,11 @@ import { StyledHome } from './Home.styled'
 
 export const Home = () => {
   const theme = useContext(ColorContext)
-  // map through the hostels and render a card for each hostel
 
   const hostel = Hostels.map((hostel, key) => {
     return <Card hostel={hostel} key={key} theme={theme} />
   })
+  
   return <StyledHome>{hostel}</StyledHome>
 }
 
