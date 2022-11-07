7d906a0c22e3f89521aec8e6ce01181883df57cd
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-07 17:05:35 +0300

    continua

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,17 +1,15 @@
 import { Card } from '../../Components'
 import Hostels from '../../Data/Hostels'
-import { ColorContext } from '../../Context'
-import { useContext } from 'react'
+import { useColor } from '../../Context'
 import { StyledHome } from './Home.styled'
 
 export const Home = () => {
-  const theme = useContext(ColorContext)
-
+  const { theme } = useColor()
   const hostel = Hostels.map((hostel, key) => {
     return <Card hostel={hostel} key={key} theme={theme} />
   })
-  
-  return <StyledHome>{hostel}</StyledHome>
+
+  return <StyledHome theme={theme}>{hostel}</StyledHome>
 }
 
 export default Home
