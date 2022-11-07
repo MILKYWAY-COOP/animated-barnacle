c3e53d62a2cf9a9daaf2d8581781bbb596ee6a5c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-07 17:05:02 +0300

    continua

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './Context'
+import { AuthContextProvider, ColorProvider } from './Context'
+import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <ColorProvider>
+          <App />
+        </ColorProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
