c48e4552b236613af4286b664c2d7c8dfe1c01bc
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-07 17:05:22 +0300

    continua

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,3 +1,3 @@
-export { ColorContext } from './ColorContext/colorContext'
+export { ColorProvider, useColor } from './ColorContext/colorContext'
 export { AuthContextProvider, useData } from './AuthContext/AuthContext'
-export {auth} from './Firebase/index'
+export { auth } from './Firebase/index'
