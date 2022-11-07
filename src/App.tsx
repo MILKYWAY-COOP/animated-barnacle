08914d77058dd9838c04ab061e88dc6905b50e70
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-07 17:04:55 +0300

    continua

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { ReactNode, useContext } from 'react'
-import { Navbar, StyledApp } from './Components'
+import { ReactNode } from 'react'
+import { Navbar } from './Components'
 import { AuthRoutes, ROUTES } from './Routes'
 
-import { ColorContext, useData } from './Context'
+import { useData } from './Context'
 
 const Wrapper = ({ children }: { children: ReactNode }) => {
-  const theme = useContext(ColorContext)
   return (
     <>
       <Navbar />
-      <StyledApp theme={theme}>{children}</StyledApp>
+      {children}
     </>
   )
 }
